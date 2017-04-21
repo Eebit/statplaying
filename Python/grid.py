5e99d3c46031954c881468092a38c212337516c9
Jake Fryer <eight_bit_hero@yahoo.ca>
2017-04-21 18:58:24 -0400

    added methods to populate the grid from a given file

--- Python/grid.py
+++ Python/grid.py
@@ -1,12 +1,26 @@
+import json
+
+def get_cell_data(filepath):
+    try:
+        with open(filepath) as data_file:
+            data = json.load(data_file)
+        return data
+    except IOError as e:
+        print("Cannot open " + filepath)
+
+
 class Cell:
-    def __init__(self, name, id, desc):
-        self.cellName = name
-        self.identifier = id
-        self.desc = desc
+    def __init__(self, cell_dict):
+        self.properties = cell_dict
+        
+        #self.cellName = name
+        #self.identifier = id
+        #self.desc = desc
     
     def __str__(self):
-        return self.identifier
+        return self.properties["cell-id"]
     
+    """
     def set_exists(self):
         self.exists = True
     
@@ -24,6 +38,7 @@ class Cell:
     
     def set_has_stats(self):
         self.hasStats = False
+    """
 
 
 class Grid:
@@ -31,11 +46,13 @@ class Grid:
         self.width = width # width = number of columns
         self.height = height # height = number of rows
         
+        d = get_cell_data('cell_bank.json')
+        
         self.grid = []
         for row in range(height):
             rowList = []
             for col in range(width):
-                rowList.append(Cell("Empty Cell", " ", ""))
+                rowList.append(Cell(d["global-cells"][0]))
             self.grid.append(rowList)
         
         #self.add_cell(Cell("lol", "l", ""), (2,4))
@@ -48,14 +65,12 @@ class Grid:
             out2 = []
             for col in range(self.width):
                 # test if the Cell is an empty cell for printing the "basic" grid
-                if(self.grid[row][col].identifier == " "):
+                if(self.grid[row][col].properties["cell-id"] == " "):
                     out2.append("-") # prints a dash for empty cell rather than a space (for visibility)
                 else:
                     out2.append(str(self.grid[row][col]))
             out.append(out2)
         
-        #out = [[str(self.grid[x][y]) for x in range(self.width)] for y in range(self.height)]
-        #out.reverse()
         return '\n'.join([''.join(x) for x in out])
     
     
@@ -66,3 +81,27 @@ class Grid:
     def add_cell(self, cell, location):
         self.grid[location[0]][location[1]] = cell
     
+    # iterate over the grid_data list of lists returned by read_grid()
+    def populate_grid_from_file(self, data, grid_data):
+        for list in range(len(grid_data)):
+            for cell in range(len(grid_data[list])):
+                found = False # set our "found" flag to false over each new symbol
+                #print("trying " + str(grid_data[list][cell]) + " at (" + str(list) + ", " + str(cell) + "):")
+                
+                # iterate through the currently-defined Cell Database JSON file (passed as "data")
+                for x in range(len(data["global-cells"])):
+                    # if we hit the "null" in the array, break the loop; no Cell defined
+                    if not data["global-cells"][x]:
+                        break
+                    # if the grid_data char is the empty cell, we modify its symbol so that we can parse it properly
+                    if(grid_data[list][cell] == "-"):
+                        grid_data[list][cell] = " "
+                    # if the current symbol is equal to something in our database, we add it to the grid
+                    # using its current coordinates (i.e. the index of the array)
+                    if(grid_data[list][cell] == data["global-cells"][x]["cell-id"]):
+                        self.add_cell(Cell(data["global-cells"][x]), (list,cell))
+                        found = True
+                
+                if(found == False):
+                    print("Could not find data for {" + grid_data[list][cell] + "} in JSON cell bank.")
+        
--- Python/gridDisplay.py
+++ Python/gridDisplay.py
@@ -17,39 +17,61 @@ class GridDisplay:
         out.append(["-" + str(col + 1) + "-" for col in range(self.grid.width)])
         
         return ('\n').join([' '.join(x) for x in out])
+
+"""
+Method that takes a filepath to a valid Grid *.txt file as its parameter,
+then reads it character by character and parses that into a list of lists
+for easy Grid construction.
+"""
+def read_grid(filepath):
+    # read a grid from a txt file and use that to populate our grid
+    try:
+        file = open(filepath, "r")
+        list = []
+        list2 = []
         
+        # loops until it sees that there is no further characters available, i.e. hits EOF 
+        while True:
+            # read in each byte at a time
+            ch = file.read(1)
+            
+            # hits EOF
+            if not ch:
+                list.append(list2) # append the last inner list before breaking
+                break
+            
+            # if it sees a newline char, it terminates the inner list and appends it to the outer list
+            elif(ch == "\n"):
+                list.append(list2)
+                list2 = [] # restart the inner list
+            
+            else:
+                list2.append(ch)
+        return list
+        
+    except IOError as e:
+        print("Cannot open " + filepath)
+        return []
+
 #######################################
 # TEST AREA
 #######################################
 
 if __name__ == "__main__":
     print("hello world")
-    lol = Grid(9, 9)
-    print(str(lol))
+    newGrid = Grid(9, 9)
+    print(str(newGrid))
     
-    with open('cell_bank.json') as data_file:
-        data = json.load(data_file)
-        
-    print(data["global-cells"][2])
+    data = get_cell_data('cell_bank.json')
     
-    # lol this is going to be ugly
-    # This is where I add a bunch of Cells to populate my "lol" Grid, based on Recursion's 1.1
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (0,0))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (0,1))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (1,0))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (2,0))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (3,0))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (4,0))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (5,0))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (6,0))
-    lol.add_cell(Cell(data["global-cells"][2]["cell-name"], data["global-cells"][2]["cell-id"], ""), (7,0))
-    lol.add_cell(Cell(data["global-cells"][3]["cell-name"], data["global-cells"][3]["cell-id"], ""), (8,0))
+    grid_data = read_grid("state_rec_1.txt")
+    print(grid_data)
     
-    lul = GridDisplay(lol)
-    print(str(lul))
+    newGrid.populate_grid_from_file(data, grid_data)
     
-    file = open("state_rec_1.txt", "r")
-    for line in file:
-        line = line.replace("\n", "")
-        print(line)
+    print(newGrid)
+    
+    display = GridDisplay(newGrid)
+    print(str(display))
+    
     
