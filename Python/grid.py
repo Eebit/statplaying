367b05c51c153a56acafee4bc78579c41f54ef3a
Jake Fryer <eight_bit_hero@yahoo.ca>
2017-05-04 16:38:10 -0400

    updating unitformat JSON, moved some methods around

--- Python/grid.py
+++ Python/grid.py
@@ -7,39 +7,49 @@ def get_cell_data(filepath):
         return data
     except IOError as e:
         print("Cannot open " + filepath)
+        
+"""
+Method that takes a filepath to a valid Grid *.txt file as its parameter,
+then reads it character by character and parses that into a list of lists
+for easy Grid construction.
+"""
+def read_grid(filepath):
+    # read a grid from a txt file and use that to populate our grid
+    try:
+        file = open(filepath, "r")
+        list = []
+        list2 = []
+        
+        # loops until it sees that there is no further characters available, i.e. hits EOF 
+        while True:
+            # read in each byte at a time
+            ch = file.read(1)
+            
+            # hits EOF
+            if not ch:
+                list.append(list2) # append the last inner list before breaking
+                break
+            
+            # if it sees a newline char, it terminates the inner list and appends it to the outer list
+            elif(ch == "\n"):
+                list.append(list2)
+                list2 = [] # restart the inner list
+            
+            else:
+                list2.append(ch)
+        return list
+        
+    except IOError as e:
+        print("Cannot open " + filepath)
+        return []
 
 
 class Cell:
     def __init__(self, cell_dict):
         self.properties = cell_dict
-        
-        #self.cellName = name
-        #self.identifier = id
-        #self.desc = desc
-        
-    
+            
     def __str__(self):
         return self.properties["cell-id"]
-    
-    """
-    def set_exists(self):
-        self.exists = True
-    
-    def set_passable(self):
-        self.passable = True
-    
-    def set_occupiable(self):
-        self.occupiable = True
-    
-    def set_destructable(self):
-        self.destructable = False
-    
-    def set_block_property(self):
-        self.blockProperty = False
-    
-    def set_has_stats(self):
-        self.hasStats = False
-    """
 
 
 class Grid:
@@ -48,7 +58,7 @@ class Grid:
         self.height = height # height = number of rows
         
         d = get_cell_data('cell_bank.json')
-        
+                
         self.grid = []
         for row in range(height):
             rowList = []
@@ -56,8 +66,6 @@ class Grid:
                 rowList.append(Cell(d["global-cells"][0]))
             self.grid.append(rowList)
         
-        #self.add_cell(Cell("lol", "l", ""), (2,4))
-        #self.grid[2][4] = Cell("lol", "l", "")
     
     def __str__(self):
         out = []
@@ -67,7 +75,10 @@ class Grid:
             for col in range(self.width):
                 # test if the Cell is an empty cell for printing the "basic" grid
                 if(self.grid[row][col].properties["cell-id"] == " "):
-                    out2.append("-") # prints a dash for empty cell rather than a space (for visibility)
+                    if(self.grid[row][col].properties["exists"] == True):
+                        out2.append("-") # prints a dash for empty cell rather than a space (for visibility)
+                    else:
+                        out2.append(" ")
                 else:
                     out2.append(str(self.grid[row][col]))
             out.append(out2)
@@ -82,26 +93,30 @@ class Grid:
     def add_cell(self, cell, location):
         self.grid[location[0]][location[1]] = cell
     
-    # iterate over the grid_data list of lists returned by read_grid()
+    """
+    Iterate over the grid_data list of lists returned by read_grid()
+    """
     def populate_grid_from_file(self, data, grid_data):
         for list in range(len(grid_data)):
             for cell in range(len(grid_data[list])):
                 found = False # set our "found" flag to false over each new symbol
-                #print("trying " + str(grid_data[list][cell]) + " at (" + str(list) + ", " + str(cell) + "):")
                 
                 # iterate through the currently-defined Cell Database JSON file (passed as "data")
                 for x in range(len(data["global-cells"])):
                     # if we hit the "null" in the array, break the loop; no Cell defined
                     if not data["global-cells"][x]:
                         break
+                    
                     # if the grid_data char is the empty cell, we modify its symbol so that we can parse it properly
                     if(grid_data[list][cell] == "-"):
                         grid_data[list][cell] = " "
+                    
                     # if the current symbol is equal to something in our database, we add it to the grid
                     # using its current coordinates (i.e. the index of the array)
                     if(grid_data[list][cell] == data["global-cells"][x]["cell-id"]):
                         self.add_cell(Cell(data["global-cells"][x]), (list,cell))
                         found = True
+                        break # break the loop so we don't keep looking after adding
                 
                 if(found == False):
                     print("Could not find data for {" + grid_data[list][cell] + "} in JSON cell bank.")
--- Python/gridDisplay.py
+++ Python/gridDisplay.py
@@ -11,47 +11,15 @@ class GridDisplay:
             
             out2 = []
             for col in range(self.grid.width):
-                out2.append("{" + str(self.grid.grid[row][col]) + "}")
+                if(self.grid.grid[row][col].properties["exists"] == True):
+                    out2.append("{" + str(self.grid.grid[row][col]) + "}")
+                else:
+                    out2.append("   ")
             out2.append(chr(row + 65)) # chr(row+65) generates the ASCII char for the capital letter
             out.append(out2)
         out.append(["-" + str(col + 1) + "-" for col in range(self.grid.width)])
         
         return ('\n').join([' '.join(x) for x in out])
-
-"""
-Method that takes a filepath to a valid Grid *.txt file as its parameter,
-then reads it character by character and parses that into a list of lists
-for easy Grid construction.
-"""
-def read_grid(filepath):
-    # read a grid from a txt file and use that to populate our grid
-    try:
-        file = open(filepath, "r")
-        list = []
-        list2 = []
-        
-        # loops until it sees that there is no further characters available, i.e. hits EOF 
-        while True:
-            # read in each byte at a time
-            ch = file.read(1)
-            
-            # hits EOF
-            if not ch:
-                list.append(list2) # append the last inner list before breaking
-                break
-            
-            # if it sees a newline char, it terminates the inner list and appends it to the outer list
-            elif(ch == "\n"):
-                list.append(list2)
-                list2 = [] # restart the inner list
-            
-            else:
-                list2.append(ch)
-        return list
-        
-    except IOError as e:
-        print("Cannot open " + filepath)
-        return []
 
 #######################################
 # TEST AREA
