0fcff42d821b57ed66eb1c05c5768a33e1927401
Gabriel Garcez Barros Sousa <gabrielgarcezbsousa@gmail.com>
2017-05-25 03:15:11 -0300

    Basic getMovementRange func
    Does not take other Units into account yet (Must study code more for this).
    
    ...It's also untested because I couldn't figure out an easy way to test it!

--- Python/cell.py
+++ Python/cell.py
@@ -36,3 +36,56 @@ class Unit(Cell):
     def output(self):
         outStr = str(self.properties["cell-name"]) + " (" + str(self.properties["profession"]["name"]) + ")" +"\nHP:\t\t" + str(self.stats["current-health"]) + "/" + str(self.stats["max-health"]) + "\nMP:\t\t" + str(self.stats["current-mana"]) + "/" + str(self.stats["max-mana"]) + "\nAtk:\t\t" + str(self.stats["attack"]) +"\nDef:\t\t" + str(self.stats["defense"]) + "\nInt:\t\t" + str(self.stats["intelligence"]) + "\nSpr:\t\t" + str(self.stats["spirit"]) + "\nCritical:\t" + str(self.stats["critical"]) + "%" + "\nEvasion:\t" + str(self.stats["evasion"]) + "%" + "\nMovement:\t" + str(self.stats["movement"]) + " Cells" + "\n{/////} {/////} {/////}"
         return outStr
+
+
+    #Takes in the grid and the entire cell dictionary does not account for other Units yet
+    def getMovementRange(self, grid, full_cell_dict):
+        mov = self.stats["movement"]
+        cur = (mov, [self.position])
+        globalCells = full_cell_dict["global-cells"]
+
+        cellByID = {}
+        for cellType in globalCells:
+            cellByID[cellType["cell-id"]] = cellType
+
+        paths = []
+        paths.append(cur)
+        possiblePaths = []
+        while(paths != []):
+            curMov, curPath = paths.pop()
+            curPos = curPath[-1]
+            curCell = grid[curPos[0]][curPos[1]]
+
+            #If you have 0 moves or more and the current cell is occupiable, then store the current path
+            if( (curMov >= 0) and (cellByID[curCell]["occupiable"] == True) and (curPath not in possiblePaths)):
+                possiblePaths.append(curPath)
+
+            #If the current path ran out of moves or isn't passable, go to the next path
+            if(curMov == 0 or cellByID[curCell]["passable"] == False): #TODO: Test for equal alignment here too
+                continue
+
+            #Try to move up
+            nextCell = (curPos[0]+1, curPos[1])
+            if(nextCell[0] < len(grid) and nextCell[1] < len(grid[0])):
+                newPath = curPath.append(nextCell[:])
+                paths.append(mov-1, newPath[:])
+            
+            #Try to move down
+            nextCell = (curPos[0]-1, curPos[1])
+            if(nextCell[0] < len(grid) and nextCell[1] < len(grid[0])):
+                newPath = curPath.append(nextCell[:])
+                paths.append(mov-1, newPath[:])
+            
+            #Try to move to the right
+            nextCell = (curPos[0], curPos[1]+1)
+            if(nextCell[0] < len(grid) and nextCell[1] < len(grid[0])):
+                newPath = curPath.append(nextCell[:])
+                paths.append(mov-1, newPath[:])
+            
+            #Try to move to the left
+            nextCell = (curPos[0], curPos[1]-1)
+            if(nextCell[0] < len(grid) and nextCell[1] < len(grid[0])):
+                newPath = curPath.append(nextCell[:])
+                paths.append(mov-1, newPath[:])
+
+        return possiblePaths
